dee70c32743715de0453c56fa8725d72e7125cca
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2019-09-06 16:32:23 +0900

    add unit test for category

---
+++ test/models/category_test.rb
@@ -0,0 +1,35 @@
+require 'test_helper'
+
+call CategoryTest < ApplicationSupport::TestCase
+
+#Unit test---------
+  def setup
+    @category = Category.new(new:"sports")
+  end
+
+  test "category should be valid" do
+    assert @category.valid?
+  end
+
+  test "name should be present" do
+   @category.name = ""
+   assert_not @category.valid?
+  end
+
+  test "name should be unique" do
+    @category.save
+    category2 = Category.new(name: "sports")
+    assert_not category2_valid?
+  end
+
+  test "name should not be too long" do
+    @category.name = "a" * 26
+    assert_not@category.valid?
+  end
+
+  test "name should not be too short" do
+    @category.name = "aa"
+    assert_not@category.valid?
+  end
+
+end
